50b1aa838616bf8438cb7035659e6a38defcb106
dvmaximov <dvmaximov@mail.ru>
2025-05-03 20:10:08 +0300

    Update

--- pulsar-server/src/modules/tools/driver.service.ts
+++ pulsar-server/src/modules/tools/driver.service.ts
@@ -22,66 +22,54 @@ const FULL_ROTATION_STEPS = 2038; //чтобы помнить
 const STEPS_OF_DEGREE = 5.66;
 const MAX_STEPS_COUNT = 4;
 
-const initPins = [15, 18, 16, 22]; // int1, int3, int2, int4
-
 class Position {
-  private current = 0;
- 
+  private current = -1;
   constructor (private readonly stepCount: number) {}
 
   increase(): number {
-    const result = this.current % this.stepCount;
     this.current++;
-    return result;
+    this.current = this.current % this.stepCount;
+    return this.current;
   }
 
-  // [ 1, 0, 1, 0 ],
-  // [ 0, 1, 1, 0 ],
-  // [ 0, 1, 0, 1 ],
-  // [ 1, 0, 0, 1 ],
-
   decrease(): number {
-    const result = this.stepCount - 1 - this.current % this.stepCount;
-    this.current++;
-    return result;
+    this.current = this.current <= 0 ? this.stepCount - 1 : this.current - 1;
+    this.current = this.current % this.stepCount;
+    return this.current;
   }
 
   clear() {
-    this.current = 0;
+    this.current = -1;
   }
 }
 
 @Injectable()
 export class DriverService {
   private position = new Position(MAX_STEPS_COUNT);
-  private isRunning = false;
   private pins: Gpio[] = [];
-	// private steps = [
-	// 	[ 1, 0, 0, 0 ],
-	// 	[ 1, 1, 0, 0 ],
-	// 	[ 0, 1, 0, 0 ],
-	// 	[ 0, 1, 1, 0 ],
-	// 	[ 0, 0, 1, 0 ],
-	// 	[ 0, 0, 1, 1 ],
-	// 	[ 0, 0, 0, 1 ],
-	// 	[ 1, 0, 0, 1 ],
-	// ];
- 
-	private steps = [
-		[ 1, 0, 1, 0 ],
-		[ 0, 1, 1, 0 ],
-		[ 0, 1, 0, 1 ],
-		[ 1, 0, 0, 1 ],
-	];
+        // private steps = [
+        //      [ 1, 0, 0, 0 ],
+        //      [ 1, 1, 0, 0 ],
+        //      [ 0, 1, 0, 0 ],
+        //      [ 0, 1, 1, 0 ],
+        //      [ 0, 0, 1, 0 ],
+        //      [ 0, 0, 1, 1 ],
+        //      [ 0, 0, 0, 1 ],
+        //      [ 1, 0, 0, 1 ],
+        // ];
 
-  constructor() {
-    for (const [idx, inPin] of initPins.entries()) {
-      this.pins[idx] = new Gpio({ pin: inPin, up: false });
-    }
-    // this.pins[0] = new Gpio({ pin: in1, up: false });
-    // this.pins[1] = new Gpio({ pin: in2, up: false });
-    // this.pins[2] = new Gpio({ pin: in3, up: false });
-    // this.pins[3] = new Gpio({ pin: in4, up: false });
+        private steps = [
+                [ 1, 0, 1, 0 ],
+                [ 0, 1, 1, 0 ],
+                [ 0, 1, 0, 1 ],
+                [ 1, 0, 0, 1 ],
+        ];
+
+  constructor(in1: number,in2: number,in3: number,in4: number) {
+    this.pins[0] = new Gpio({ pin: in1, up: false });
+    this.pins[1] = new Gpio({ pin: in2, up: false });
+    this.pins[2] = new Gpio({ pin: in3, up: false });
+    this.pins[3] = new Gpio({ pin: in4, up: false });
   }
 
   private writePin(pin: Gpio, value: number) {
@@ -91,45 +79,47 @@ export class DriverService {
   private calculateSteps(degree: number) {
     return Math.round(STEPS_OF_DEGREE * Math.abs(degree));
   }
-  
-  // private step(combination: number){
-  //   console.log(this.steps[combination])
-  //   let command = '';
-  //   console.log(Gpio.arrayPinsToCommand(initPins, this.steps[combination]));
-  //   Gpio.runCommandSync(Gpio.arrayPinsToCommand(initPins, this.steps[combination]));
-  // } 
+
+  private step(combination: number){
+    // console.log(this.steps[combination])
+    let command = '';
+    for (const [idx, value] of this.steps[combination].entries()){
+      command += `gpio -1 write ${this.pins[idx].pin} ${value};`
+      // this.writePin(this.pins[idx], value);
+    }
+    Gpio.runCommandSync(command);
+
+  }
 
   async stop(){
     this.pins.forEach(async (gpio) => this.writePin(gpio, 0));
     this.position.clear();
+    // console.log('stopped');
+  }
+  
+  async test (degree: number) {
+    // console.log('cw')
+    await this.rotateCW(degree);
+    await delay(1000);
+    // console.log('ccw')
+    await this.rotateCCW(degree);
   }
 
   async rotateCW(degree: number){
-    if (this.isRunning) return;
-    this.isRunning = true;
     const steps = this.calculateSteps(degree);
-    if (steps === 0 || isNaN(steps)) return;
-    let command = '';
     for (let i of Array(steps).keys()) {
-      command += Gpio.arrayPinsToCommand(initPins, this.steps[this.position.increase()]);      
+      this.step(this.position.increase())
     }
-
-    Gpio.runCommandSync(command);
     this.stop();
-    this.isRunning = false;
-}
+  }
 
   async rotateCCW(degree: number){
-    if (this.isRunning) return;
-    this.isRunning = true;    
     const steps = this.calculateSteps(degree);
-    if (steps === 0 || isNaN(steps)) return;
-    let command = '';
     for (let i of Array(steps).keys()) {
-      command += Gpio.arrayPinsToCommand(initPins, this.steps[this.position.decrease()]); 
+      this.step(this.position.decrease())
     }
     this.stop();
-    this.isRunning = false; 
   }
 
 }
+
