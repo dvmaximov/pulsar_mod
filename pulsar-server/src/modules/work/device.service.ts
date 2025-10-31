e69deeb7a9eb4133cc45663517a3b0d1b9992b76
dvmaximov <dvmaximov@mail.ru>
2025-10-31 14:24:03 +0300

    Update

--- pulsar-server/src/modules/work/device.service.ts
+++ pulsar-server/src/modules/work/device.service.ts
@@ -35,7 +35,8 @@ export class DeviceService {
     this.driveRight = new Gpio({ pin: PIN.PIN_RIGHT });
     this.driveUp = new Gpio({ pin: PIN.PIN_UP });
     this.driveDown = new Gpio({ pin: PIN.PIN_DOWN });
-    this.spark = new Gpio({ pin: PIN.PIN_SPARK, up: false });
+    // this.spark = new Gpio({ pin: PIN.PIN_SPARK, up: false });
+    this.spark = new Gpio({ pin: PIN.PIN_SPARK });
   }
 
   private async delay(ms): Promise<any> {
